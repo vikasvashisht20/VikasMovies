db64e21595ac561fc9dd27a631681a8342808651
vikasvashisht20 <vashishtvikas1996@gmail.com>
2023-10-11 23:50:58 +0530

    added https instead of http

--- src/contex.js
+++ src/contex.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect, useState } from "react";
 
-export const API_URL = `http://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
+export const API_URL = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_API_KEY}`;
 
 // fist we need to create context
 const AppContext = React.createContext();
